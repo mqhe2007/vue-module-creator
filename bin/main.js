b1ae0eba00ca6390bc00a49ae0cc90a783b6cfa2
青衿 <122274389@qq.com>
2019-05-17 15:40:34 +0800

    初次提交

---
+++ bin/main.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const program = require('commander')
+const resolveCommand = command => path.resolve(__dirname, '../command', command)
+program.version(require('../package.json').version)
+program
+  .command('init')
+  .description('创建一个新项目')
+  .alias('i')
+  .action(() => {
+    require(resolveCommand('init'))
+  })
+if (!process.argv.slice(2).length) {
+  program.help()
+}
+program.parse(process.argv)
---
+++ command/init.js
@@ -0,0 +1,81 @@
+const fs = require('fs')
+const program = require('commander')
+const inquirer = require('inquirer')
+const ora = require('ora')
+const download = require('download-git-repo')
+const chalk = require('chalk')
+const nameOption = program.parse(process.argv).args[0]
+const defaultName = 'admincraft-project'
+const templateRepository = 'mqhe2007/admincraft-showcase'
+const loading = ora('模板下载中，请稍后……')
+const questions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: '项目名称',
+    default: typeof nameOption === 'string' ? nameOption : defaultName,
+    filter: value => value.trim(),
+    validate: value => {
+      const validate = value.trim().split(' ').length === 1
+      return validate || '项目名称不允许有空格！'
+    },
+    transformer: value => `：${value}`
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: '项目描述',
+    default: 'a admincraft project',
+    validate: () => {
+      return true
+    },
+    transformer: value => `：${value}`
+  },
+  {
+    type: 'input',
+    name: 'author',
+    message: '项目作者',
+    default: 'unnamed',
+    validate(val) {
+      return true
+    },
+    transformer: value => `：${value}`
+  }
+]
+module.exports = inquirer
+  .prompt(questions)
+  .then(({ name, description, author }) => {
+    loading.start()
+    download(templateRepository, `./${name}`, err => {
+      if (err) {
+        loading.fail('模板下载失败！')
+        console.log(chalk.red(err))
+        process.exit()
+      } else {
+        fs.readFile(`./${name}/package.json`, 'utf8', (err, data) => {
+          if (err) {
+            console.log(chalk.red(err))
+            process.exit()
+          }
+          const packageJson = JSON.parse(data)
+          packageJson.name = name
+          packageJson.description = description
+          packageJson.author = author
+          packageString = JSON.stringify(packageJson, null, 2)
+          fs.writeFile(`./${name}/package.json`, packageString, 'utf8', err => {
+            if (err) {
+              console.log(chalk.red(err))
+              process.exit()
+            }
+            loading.succeed('模板准备就绪！请进一步操作。')
+            console.log(`
+            ${chalk.bgWhite.black('  进一步操作  ')}
+            ${chalk.yellow(`cd ${name}`)}
+            ${chalk.yellow('yarn 或 npm install')}
+            ${chalk.yellow('yarn serve 或 npm run serve')}
+          `)
+          })
+        })
+      }
+    })
+  })
