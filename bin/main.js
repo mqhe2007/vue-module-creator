c1304167eb68f99e84229c3aaee29a986ef38fd9
孟庆贺 <122274389@qq.com>
2023-06-13 12:50:24 +0800

    版本2.2.1

--- bin/main.js
+++ bin/main.js
@@ -2,15 +2,26 @@
 import { program } from 'commander'
 import { create } from '../command/create.js'
 import { generate } from '../command/generate.js'
-program.name('vml').description('专注于Vue的微前端模块生成器').version('2.0.0')
+import { readFileSync } from 'fs'
+import { resolve } from 'path'
+const pkg = JSON.parse(
+  readFileSync(resolve(process.cwd(), './package.json'), 'utf-8')
+)
+program
+  .name('vml')
+  .description('专注于Vue的微前端模块生成器')
+  .option('-h, --help', '查看帮助')
+  .version(pkg.version, '-v, --version', '查看版本号')
 program
   .command('create')
+  .alias('c')
   .description('创建一个新项目')
   .argument('[projectName]', '项目名称。', 'vue-module')
   .action(create)
 program
-  .command('g')
-  .description('g是generate的简写，代表生成命令。')
+  .command('generate')
+  .alias('g')
+  .description('生成模板文件。')
   .argument('<moduleName>', '模块名称，支持相对路径方式。')
   .option('-t, --type <stype>', '模块类型，可选app或module，默认为app', 'app')
   .action(generate)
